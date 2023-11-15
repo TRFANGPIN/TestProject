bb7bf1a9252056ce708121c5407bebbbab88029e
TRFANGPIN <108950275+TRFANGPIN@users.noreply.github.com>
2023-11-15 20:49:25 +0400

    validator updated

--- src/middleware/validator.js
+++ src/middleware/validator.js
@@ -1,10 +1,9 @@
 const objectId = require("mongoose").Types.ObjectId;
 
 module.exports.checkobject = (req) => {
-  if (Object.keys(req).length > 0) {
+  if (req != null && Object.keys(req).length > 0) {
     return true;
   } else {
     return false;
   }
 };
-
